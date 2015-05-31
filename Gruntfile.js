79dd7a450cc3954774eea395f4ec43cb8cc7dbd4
ThomasJ <tom@axisj.com>
2015-06-01 01:36:15 +0900

    모달정리등 01:36

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,20 +13,46 @@
 				'-->\n',
 				separator: '\n\n<!-- split -->\n\n'
 			},
-			core: {
+			intro: {
 				src: [
 					'samples/layout/head.html',
 					'samples/layout/visual-dom.html',
-					'samples/axisj/core/*.html',
+					'samples/axisj/intro/*.html',
 					'samples/layout/bottom.html'
 				],
 				dest: 'samples/index.html'
+			},
+			chapter1: {
+				src: [
+					'samples/layout/head.html',
+					'samples/layout/visual-dom.html',
+					'samples/axisj/chapter1/*.html',
+					'samples/layout/bottom.html'
+				],
+				dest: 'samples/chapter1.html'
+			},
+			chapter2: {
+				src: [
+					'samples/layout/head.html',
+					'samples/layout/visual-dom.html',
+					'samples/axisj/chapter2/*.html',
+					'samples/layout/bottom.html'
+				],
+				dest: 'samples/chapter2.html'
 			}
 		},
 		watch: {
-			core: {
-				files: ['samples/axisj/core/*.html'],
-				tasks: ['concat:core', 'replace:core']
+			intro: {
+				files: ['samples/axisj/intro/*.html'],
+				tasks: ['concat:intro', 'replace:intro']
+			},
+			chapter1: {
+				files: ['samples/axisj/chapter1/*.html'],
+				tasks: ['concat:chapter1', 'replace:chapter1']
+			},
+			chapter2: {
+				files: ['samples/axisj/chapter2/*.html'],
+				tasks: ['concat:chapter2', 'replace:chapter2']
 			},
 			sample_doc: {
 				files: ['samples/css/*.scss'],
@@ -48,8 +74,42 @@
 			}
 		},
 		replace: {
-			core: {
+			intro: {
 				src: ['samples/index.html'],
+				overwrite: true,                 // overwrite matched source files
+				options: {
+					processTemplates: false
+				},
+				replacements: [{
+					from: /<pre[^>]*>([^<]*(?:(?!<\/?pre)<[^<]*)*)<\/pre\s*>/gi,
+					to: function (matchedWord, index, fullText, regexMatches) {
+						if(regexMatches.join('').substr(0, 9) == "$noscript"){
+							return '<pre class="prettyprint linenums">'+ regexMatches.join('').replace(/\$noscript\$/g, "").replace(/</g, "&lt;") +'</pre>';
+						}else{
+							return '<pre class="prettyprint linenums">'+ regexMatches.join('').replace(/</g, "&lt;") +'</pre>' + '<h4>Result</h4>' + regexMatches.join('');
+						}
+					}
+				}]
+			},
+			chapter1: {
+				src: ['samples/chapter1.html'],
+				overwrite: true,                 // overwrite matched source files
+				options: {
+					processTemplates: false
+				},
+				replacements: [{
+					from: /<pre[^>]*>([^<]*(?:(?!<\/?pre)<[^<]*)*)<\/pre\s*>/gi,
+					to: function (matchedWord, index, fullText, regexMatches) {
+						if(regexMatches.join('').substr(0, 9) == "$noscript"){
+							return '<pre class="prettyprint linenums">'+ regexMatches.join('').replace(/\$noscript\$/g, "").replace(/</g, "&lt;") +'</pre>';
+						}else{
+							return '<pre class="prettyprint linenums">'+ regexMatches.join('').replace(/</g, "&lt;") +'</pre>' + '<h4>Result</h4>' + regexMatches.join('');
+						}
+					}
+				}]
+			},
+			chapter2: {
+				src: ['samples/chapter2.html'],
 				overwrite: true,                 // overwrite matched source files
 				options: {
 					processTemplates: false
@@ -73,6 +133,8 @@
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-sass');
 
-	grunt.registerTask('axisj-core', ['concat:core','replace:core','watch:core']);
+	grunt.registerTask('intro', ['concat:intro','replace:intro','watch:intro']);
+	grunt.registerTask('chapter1', ['concat:chapter1','replace:chapter1','watch:chapter1']);
+	grunt.registerTask('chapter2', ['concat:chapter2','replace:chapter2','watch:chapter2']);
 	grunt.registerTask('sass-make-doc', ['sass:sample_doc','watch:sample_doc']);
 };
